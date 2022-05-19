bf619ebcef1883a28aa7fd3c5b39f209b7078bbb
holyfs <egcg1986@gmail.com>
2022-05-19 10:31:43 +0000

    entrega del generador de carta aleatoria

--- src/app.js
+++ src/app.js
@@ -4,8 +4,53 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //Funcion de nueva carta cada 10 seg.
+  let footer = document.createElement("h3");
+  footer.innerHTML =
+    "This card will reset every 10 seconds, or you can push this button";
+  footer.style.position = "relative";
+  footer.style.marginLeft = "25%";
+  document.body.appendChild(footer);
+  var intervalID = window.setInterval(nuevaCarta, 10000);
+  function nuevaCarta() {
+    location.reload();
+  }
+
+  //numeroDeCarta
+  let numero = document.querySelector(".numero");
+  let numeroDeCarta = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let numeroRandom = Math.floor(Math.random() * 13);
+
+  //PaloDeCarta
+  let paloUp = document.querySelector(".paloUp");
+  let paloDown = document.querySelector(".paloDown");
+  let tipoDeCarta = ["♦", "♥", "♠", "♣"];
+  let tipoRandom = Math.floor(Math.random() * 4);
+
+  //color
+  if (tipoDeCarta[tipoRandom] === "♦" || tipoDeCarta[tipoRandom] === "♥") {
+    paloUp.classList.add("corazon");
+    paloDown.classList.add("corazon");
+    numero.classList.add("corazon");
+  }
+
+  //Boton
+  let button = document.createElement("Button");
+  button.innerHTML = "Your new card";
+  button.style.position = "absolute";
+  button.style.marginLeft = "46%";
+  document.body.appendChild(button);
+  button.addEventListener("click", event => {
+    location.reload();
+  });
+
+  //impresiones
+  paloUp.innerText = tipoDeCarta[tipoRandom];
+  paloDown.innerText = tipoDeCarta[tipoRandom];
+  numero.innerText = numeroDeCarta[numeroRandom];
 };
